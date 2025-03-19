bd1379be3f6d3a998638d3ba6f5f35e3fae6eb13
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:47:22 +0800

    final prog5.10

--- input_incorrect_name_casing_convert_to_snake.py
+++ input_incorrect_name_casing_convert_to_snake.py
@@ -1,4 +1,8 @@
 #input name
-#convert into smaller case
+name = input("\nEnter your name: ")
+#convert into lower case
+lower_cased_name = name.lower()
 #convert into snake case
+snake_casing_name = lower_cased_name.replace(" ", "_")
 #output name
+print (f"\n{snake_casing_name}")
