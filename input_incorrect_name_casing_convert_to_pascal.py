fe1f96fd975e8de1802c30a5e6ba938c71050e07
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:43:43 +0800

    final prog5.9

--- input_incorrect_name_casing_convert_to_pascal.py
+++ input_incorrect_name_casing_convert_to_pascal.py
@@ -1,4 +1,8 @@
 #input incorrect name casing
+name = input("\nEnter your name: ")
 #convert into proper casing
+proper_cased_name = name.title()
 #convert into pascal casing
+pascal_casing_name = proper_cased_name.replace(" ", "")
 #print name
+print (f"\n{pascal_casing_name}")
