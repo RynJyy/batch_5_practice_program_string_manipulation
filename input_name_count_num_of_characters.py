298ae32c637ac2d31759658b4d5383caa70c166d
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:39:39 +0800

    final prog5.8

--- input_name_count_num_of_characters.py
+++ input_name_count_num_of_characters.py
@@ -1,3 +1,6 @@
 #input name
-#count number of characters in name
+name = input("\nEnter your name: ")
+#count number of characters in name 
+number_of_characters = len(name.replace(" ", ""))
 #print number of characters
+print (f"\nNumber of characters in your name: {number_of_characters}")
