7246f1487043251682b8921c98fc588f40c2b68d
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:10:51 +0800

    change name to pseudocode prog5.2

--- input_num_0_to_1000_add_0_if_less_than_6_digits.py
+++ input_num_0_to_1000_add_0_if_less_than_6_digits.py
@@ -1,5 +1,4 @@
 #input num
-#check if it is less than 6 digits
-#add 0 to the left of the number
+#if num is less than 6 digits add 0 to the left of the number to complete 6 digits
     #output num
 #print num if 6 digits
