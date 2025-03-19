1a851489d2364c1c61887c4443e4308c8185a1ce
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:34:30 +0800

    final prog5.7

--- input_statemet_count_num_of_words.py
+++ input_statemet_count_num_of_words.py
@@ -1,3 +1,6 @@
 #input statement 
+statement = input("\nEnter a statement: ")
 #count number of words
+number_of_words = len(statement.split())
 #print number of words
+print (f"\nNumber of words in the statement: {number_of_words}")
