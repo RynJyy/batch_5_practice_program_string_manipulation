b13d259a44d95a0e70f63b7595d52f73b9e172cf
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-20 02:13:40 +0800

    final prog5.2

--- input_num_0_to_1000_add_0_if_less_than_6_digits.py
+++ input_num_0_to_1000_add_0_if_less_than_6_digits.py
@@ -1,4 +1,10 @@
 #input num
+num = input("\nEnter a number between 0 and 1000: ")
 #if num is less than 6 digits add 0 to the left of the number to complete 6 digits
+if len(num) < 6:
+    num = '0' + num
     #output num
+    print (f"\n{num}")
 #print num if 6 digits
+else:
+    print (f"\n{num}")
